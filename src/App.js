9972cc8a8288688f841e98d16d3b591fcbc12f35
tominhthuan <tominhthuan4224@gmail.com>
2023-08-02 21:47:58 +0700

    phan trang scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
@@ -14,7 +13,10 @@ function App() {
       <DispatchProvider dispatch={store.dispatch}>
         <div className='App'>
           <Header />
-          <TodoList />
+          <div className='App-container'>
+            <TodoList />
+          </div>
+
           <Footer />
         </div>
       </DispatchProvider>
--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,5 +1,4 @@
-
-import React, { useMemo, useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Todos from './Todos';
 
@@ -7,42 +6,43 @@ function TodoList() {
     const todos = useSelector((state) => state.todos);
     const filter = useSelector((state) => state.filter);
 
-    const [itemsToShow, setItemsToShow] = useState(5);
     const itemsPerScroll = 5;
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        container.addEventListener('scroll', handleScroll);
+        return () => {
+            container.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const handleScroll = () => {
-        const scrollY = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const bodyHeight = document.body.scrollHeight;
-        const bottom = scrollY + windowHeight >= bodyHeight;
+        const container = containerRef.current;
+        const scrollY = container.scrollTop;
+        const containerHeight = container.clientHeight;
+        const contentHeight = container.scrollHeight;
+        const bottom = scrollY + containerHeight >= contentHeight;
         if (bottom) {
             setItemsToShow((prevItems) => prevItems + itemsPerScroll);
         }
     };
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    const filteredTodos = todos.filter((todo) => {
+        if (filter === 'completed') {
+            return todo.isCompleted;
+        } else if (filter === 'active') {
+            return !todo.isCompleted;
+        } else {
+            return true;
+        }
+    });
 
-    const filteredTodos = useMemo(() => {
-        return todos.filter((todo) => {
-            if (filter === 'completed') {
-                return todo.isCompleted;
-            } else if (filter === 'active') {
-                return !todo.isCompleted;
-            } else {
-                return true;
-            }
-        });
-    }, [todos, filter]);
-
+    const [itemsToShow, setItemsToShow] = React.useState(itemsPerScroll);
     const currentTodos = filteredTodos.slice(0, itemsToShow);
 
     return (
-        <div>
+        <div ref={containerRef} className='Todolists'>
             {currentTodos.map((item, index) => (
                 <Todos key={item.id} item={item} index={index} />
             ))}
